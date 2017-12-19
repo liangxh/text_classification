6703d4f59413db02d907d454e316546e6389ed7a
xihaoliang <xihao.liang@ihandysoft.com>
2017-12-19 19:30:32 +0800

    vocab moved outside

--- model/config.py
+++ model/config.py
@@ -5,7 +5,6 @@ import os
 class Config(object):
     path_to_root = os.path.join(os.path.dirname(__file__), '..')
     path_to_task2 = os.path.join(os.environ['HOME'], 'lab/task2')
-    path_to_local = os.path.join(path_to_root, 'local')
     path_to_glove_dir = os.path.join(os.environ['HOME'], 'lab/glove')
 
     def path_to_glove(self, key, dim):
@@ -15,9 +14,7 @@ class Config(object):
         return os.path.join(self.path_to_glove_dir, '{}.{}d.index'.format(key, dim))
 
     def path_to_vocab(self, key):
-        return os.path.join(self.path_to_local, 'task2.{}.vocab'.format(key))
+        return os.path.join(self.path_to_task2, 'vocab/{}'.format(key))
 
-    def path_to_tokenized(self, key):
-        return os.path.join(self.path_to_local, 'task2.{}.tokenized'.format(key))
 
 config = Config()
