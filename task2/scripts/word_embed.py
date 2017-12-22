066a61e9d8d9d7f3182a2e481c07569b0ea299b6
xihaoliang <xihao.liang@ihandysoft.com>
2017-12-22 19:01:25 +0800

    bug fixed

--- task2/scripts/word_embed.py
+++ task2/scripts/word_embed.py
@@ -8,10 +8,11 @@ from task2.model.config import config
 @commandr.command('fg')
 def build_frederic_godin_index(key, n):
     from nlp.lib.word_embed.word2vec.frederic_godin import FredericGodinModel
+    n = int(n)
 
     vocab_list = task2.dataset.load_vocab(key, n)
     index = FredericGodinModel.build_specific_index(vocab_list)
-    json.dumps(index, open(config.path_to_frederic_godin_index(key), 'w'))
+    json.dump(index, open(config.path_to_frederic_godin_index(key), 'w'))
 
 
 if __name__ == '__main__':
