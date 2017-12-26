ad49ec8ba984eca41995812fe2fd9e364cf1c601
xihaoliang <xihao.liang@ihandysoft.com>
2017-12-26 20:46:54 +0800

    <feat> simple gru with attention added

---
+++ task2/nn/gru_attention.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+from task2.model import const
+from task2.nn.base import BaseAlgorithm
+from task2.nn.pack import NNPack
+
+
+class Algorithm(BaseAlgorithm):
+    @classmethod
+    def build_from_graph(cls, graph):
+        other_keys = [
+            const.LABEL_PREDICT,
+            const.LABEL_GOLD,
+            const.DROPOUT_KEEP_PROB,
+            const.LOSS,
+            const.GLOBAL_STEP,
+            const.OPTIMIZER,
+            'rnn/transpose'
+        ]
+        ph_input = cls._get_from_graph(graph, cls.feed_keys_input)
+        kwargs = cls._get_from_graph(graph, other_keys)
+
+        return NNPack(ph_input=ph_input, **kwargs)
+
+    def build_neural_network(self, lookup_table):
+        token_id_seq = tf.placeholder(tf.int32, [None, self.config.seq_len], name=const.TOKEN_ID_SEQ)
+        lexicon_feat = tf.placeholder(tf.float32, [None, self.config.dim_lexicon_feat], name=const.LEXICON_FEAT)
+        seq_len = tf.placeholder(tf.int32, [None, ], name=const.SEQ_LEN)
+        dropout_keep_prob = tf.placeholder(tf.float32, name=const.DROPOUT_KEEP_PROB)
+        lookup_table = tf.Variable(lookup_table, dtype=tf.float32, name=const.LOOKUP_TABLE)
+
+        embedded = tf.nn.embedding_lookup(lookup_table, token_id_seq)
+        rnn_outputs, rnn_last_states = tf.nn.dynamic_rnn(
+            tf.nn.rnn_cell.GRUCell(self.config.dim_rnn),
+            inputs=embedded,
+            sequence_length=seq_len,
+            dtype=tf.float32
+        )
+
+        def attention(inputs, attention_size):
+            inputs_shape = inputs.shape
+            seq_length = inputs_shape[1].value
+            hidden_size = inputs_shape[2].value
+
+            w = tf.Variable(tf.truncated_normal([hidden_size, attention_size], stddev=0.1))
+            b = tf.Variable(tf.truncated_normal([1, attention_size], stddev=0.1))
+            u = tf.Variable(tf.truncated_normal([attention_size, 1], stddev=0.1))
+
+            v = tf.tanh(tf.matmul(tf.reshape(inputs, [-1, hidden_size]), w)) + b  # [batch_size*seq_len, attention_size]
+            vu = tf.matmul(v, u)                                                # [batch_size * seq_len, 1]
+            exps = tf.reshape(tf.exp(vu), [-1, seq_length])                     # [batch_size, seq_len]
+            alphas = exps / tf.reshape(tf.reduce_sum(exps, 1), [-1, 1])         # /[batch_size] -> [batch_size, seq_len]
+            output = tf.reduce_sum(
+                        inputs * tf.reshape(alphas, [-1, seq_length, 1]), 1)
+            return output, alphas
+
+        attention_output, _ = attention(rnn_outputs, self.config.attention_size)
+
+        dense_input = tf.concat([attention_output, lexicon_feat], axis=1)
+
+        w = tf.Variable(tf.truncated_normal([dense_input.shape[-1].value, self.config.dim_output], stddev=0.1))
+        b = tf.Variable(tf.constant(0.1, shape=[self.config.dim_output]))
+        y = tf.matmul(dense_input, w) + b
+
+        # 預測標籤
+        label_predict = tf.cast(tf.argmax(y, 1), tf.int32, name=const.LABEL_PREDICT)
+
+        # 計算loss
+        label_gold = tf.placeholder(tf.int32, [None, ], name=const.LABEL_GOLD)
+        prob_gold = tf.one_hot(label_gold, self.config.dim_output)
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=prob_gold), name=const.LOSS)
+        l2_loss = tf.constant(0., dtype=tf.float32)
+        l2_loss += tf.nn.l2_loss(w)
+        loss += self.config.l2_reg_lambda * l2_loss
+
+        global_step = tf.Variable(0, trainable=False, name=const.GLOBAL_STEP)
+        learning_rate = tf.train.exponential_decay(
+            self.config.learning_rate_initial,
+            global_step=global_step,
+            decay_steps=self.config.learning_rate_decay_steps,
+            decay_rate=self.config.learning_rate_decay_rate
+        )
+        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step, name=const.OPTIMIZER)
+
+        return self.build_from_graph(tf.get_default_graph())
--- task2/nn/pack.py
+++ task2/nn/pack.py
@@ -6,7 +6,7 @@ class NNPack(object):
                  ph_input,
                  dropout_keep_prob=None,
                  label_gold=None, label_predict=None,
-                 loss=None, optimizer=None, global_step=None
+                 loss=None, optimizer=None, global_step=None, **kwargs
                  ):
         self.ph_input = ph_input
         self.dropout_keep_prob = dropout_keep_prob
@@ -15,6 +15,10 @@ class NNPack(object):
         self.loss = loss
         self.global_step = global_step
         self.optimizer = optimizer
+        self.other = kwargs
+
+    def get(self, key):
+        return self.other[key]
 
     def input_keys(self):
         return self.ph_input.keys()
