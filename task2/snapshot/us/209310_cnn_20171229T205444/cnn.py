9eae89c5172f30dbd21538a563d18823481ebc63
liangxh <liangxh16@mails.tsinghua.edu.cn>
2017-12-29 20:58:31 +0800

    <data> snapshot updated

---
+++ task2/snapshot/us/209310_cnn_20171229T205444/cnn.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+from task2.model import const
+from task2.nn.base import BaseAlgorithm
+from task2.nn.common import dense, cnn
+
+
+class Algorithm(BaseAlgorithm):
+    def build_neural_network(self, lookup_table):
+        token_id_seq = tf.placeholder(tf.int32, [None, self.config.seq_len], name=const.TOKEN_ID_SEQ)
+        lexicon_feat = tf.placeholder(tf.float32, [None, self.config.dim_lexicon_feat], name=const.LEXICON_FEAT)
+        seq_len = tf.placeholder(tf.int32, [None, ], name=const.SEQ_LEN)
+        dropout_keep_prob = tf.placeholder(tf.float32, name=const.DROPOUT_KEEP_PROB)
+        lookup_table = tf.Variable(lookup_table, dtype=tf.float32, name=const.LOOKUP_TABLE,
+                                   trainable=self.config.embedding_trainable)
+        embedded = tf.nn.embedding_lookup(lookup_table, token_id_seq)
+
+        # cnn
+        cnn_output = cnn.build(embedded, self.config.filter_num, self.config.kernel_size)
+        last_state = cnn.max_pooling(cnn_output)
+
+        dense_input = tf.concat([last_state, lexicon_feat], axis=1)
+
+        y, w, b = dense.build(dense_input, self.config.dim_output)
+
+        # 計算loss
+        label_gold = tf.placeholder(tf.int32, [None, ], name=const.LABEL_GOLD)
+        prob_gold = tf.one_hot(label_gold, self.config.dim_output)
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=prob_gold), name=const.LOSS)
+        if self.config.l2_reg_lambda is not None and self.config.l2_reg_lambda > 0:
+            l2_loss = tf.constant(0., dtype=tf.float32)
+            l2_loss += tf.nn.l2_loss(w)
+            loss += self.config.l2_reg_lambda * l2_loss
+
+        global_step = tf.Variable(0, trainable=False, name=const.GLOBAL_STEP)
+        learning_rate = tf.train.exponential_decay(
+            self.config.learning_rate_initial,
+            global_step=global_step,
+            decay_steps=self.config.learning_rate_decay_steps,
+            decay_rate=self.config.learning_rate_decay_rate
+        )
+
+        # 預測標籤
+        tf.cast(tf.argmax(y, 1), tf.int32, name=const.LABEL_PREDICT)
+
+        # Optimizer
+        tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step, name=const.OPTIMIZER)
+
+        return self.build_from_graph(tf.get_default_graph())
