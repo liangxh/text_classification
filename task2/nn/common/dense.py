7d314348e02a4d14a8bd32cedbeaaf01bfc5e503
xihaoliang <xihao.liang@ihandysoft.com>
2018-01-14 11:32:59 +0800

    <feat> gruext added

--- task2/nn/common/dense.py
+++ task2/nn/common/dense.py
@@ -2,15 +2,22 @@
 import tensorflow as tf
 
 
-def build(ph_input, dim_output, activation=None):
+def build(ph_input, dim_output, activation=None, bias=True):
     """
     由于tensorflow.layers.dense不會返回全連接層的 W和b，若L2 Loss有需要加入W和b時可以使用
     否則建議直接使用tensorflow.layers.dense
     """
     w = tf.Variable(tf.truncated_normal([ph_input.shape[-1].value, dim_output], stddev=0.1))
-    b = tf.Variable(tf.constant(0.1, shape=[dim_output]))
-    y = tf.matmul(ph_input, w) + b
+    y = tf.matmul(ph_input, w)
+
+    if bias:
+        b = tf.Variable(tf.constant(0.1, shape=[dim_output]))
+        y += b
 
     if activation is not None:
         y = activation(y)
-    return y, w, b
+
+    if bias:
+        return y, w, b
+    else:
+        return y, w
---
+++ task2/nn/gruext.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+from task2.model import const
+from task2.nn.base import BaseAlgorithm
+from task2.nn.common import dense, rnn_cell
+
+
+class Algorithm(BaseAlgorithm):
+    def build_neural_network(self, lookup_table):
+        token_id_seq = tf.placeholder(tf.int32, [None, self.config.seq_len], name=const.TOKEN_ID_SEQ)
+        lexicon_feat = tf.placeholder(tf.float32, [None, self.config.dim_lexicon_feat], name=const.LEXICON_FEAT)
+        seq_len = tf.placeholder(tf.int32, [None, ], name=const.SEQ_LEN)
+        dropout_keep_prob = tf.placeholder(tf.float32, name=const.DROPOUT_KEEP_PROB)
+
+        lookup_table = tf.Variable(lookup_table, dtype=tf.float32, name=const.LOOKUP_TABLE,
+                                   trainable=self.config.embedding_trainable)
+
+        embedded = tf.nn.embedding_lookup(lookup_table, token_id_seq)
+
+        dense_input = list()
+        for idx, dim in enumerate(self.config.dim_rnn):
+            with tf.variable_scope('rnn_{}'.format(idx)):
+                rnn_outputs, rnn_last_states = tf.nn.dynamic_rnn(
+                    rnn_cell.build_gru(dim, dropout_keep_prob=dropout_keep_prob),
+                    inputs=embedded,
+                    sequence_length=seq_len,
+                    dtype=tf.float32
+                )
+                dense_input.append(rnn_last_states)
+
+        last_lex = lexicon_feat
+        for dim in self.config.dim_lex_dense:
+            last_lex, _, _ = dense.build(last_lex, dim, activation=tf.nn.tanh)
+        dense_input.append(last_lex)
+
+        dense_input = tf.concat(dense_input, axis=1)
+        y, w, b = dense.build(dense_input, self.config.dim_output)
+
+        # 計算loss
+        label_gold = tf.placeholder(tf.int32, [None, ], name=const.LABEL_GOLD)
+        prob_gold = tf.one_hot(label_gold, self.config.dim_output)
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=prob_gold), name=const.LOSS)
+        if self.config.l2_reg_lambda is not None and self.config.l2_reg_lambda > 0:
+            l2_loss = tf.constant(0., dtype=tf.float32)
+            l2_loss += tf.nn.l2_loss(w)
+            loss += self.config.l2_reg_lambda * l2_loss
+
+        global_step = tf.Variable(0, trainable=False, name=const.GLOBAL_STEP)
+        learning_rate = tf.train.exponential_decay(
+            self.config.learning_rate_initial,
+            global_step=global_step,
+            decay_steps=self.config.learning_rate_decay_steps,
+            decay_rate=self.config.learning_rate_decay_rate
+        )
+
+        # 預測標籤
+        tf.cast(tf.argmax(y, 1), tf.int32, name=const.LABEL_PREDICT)
+
+        # Optimizer
+        tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step, name=const.OPTIMIZER)
+
+        return self.build_from_graph(tf.get_default_graph())
