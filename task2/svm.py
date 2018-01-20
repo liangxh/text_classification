f50423f79e713608753d6d89358d9eb2e0daa559
xihaoliang <xihao.liang@ihandysoft.com>
2018-01-20 23:27:06 +0800

    <feat> svm stack added

--- task2/svm.py
+++ task2/svm.py
@@ -9,6 +9,7 @@ from task2.lib import evaluate
 from scipy import sparse
 from collections import defaultdict
 import math
+from sklearn.feature_selection import SelectFromModel
 
 
 @commandr.command('linear')
@@ -142,6 +143,41 @@ def csupport(key, c=1., kernel='rbf'):
     labels_predict = model.predict(X)
     score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
     print('[TRIAL]', score_dict)
+
+
+@commandr.command('stack')
+def stack(key, c=1.):
+    """
+    線性SVM
+    輸入只用Tf-Idf
+    """
+    c = float(c)
+    tokenizer = TweetTokenizer(preserve_case=False, reduce_len=True, strip_handles=False).tokenize
+    vectorizer = TfidfVectorizer(strip_accents="unicode", analyzer="word", tokenizer=tokenizer, stop_words=None)
+
+    X = vectorizer.fit_transform(task2.dataset.load_tokenized_as_texts(key, 'train'))
+    labels_gold = task2.dataset.load_labels(key, 'train')
+
+    model = LinearSVC(C=c, verbose=1)
+    model.fit(X, labels_gold)
+    feat_transformer = SelectFromModel(model, prefit=True)
+
+    X = feat_transformer.transform(X)
+    model = LinearSVC(C=c, verbose=1)
+    model.fit(X, labels_gold)
+
+    labels_predict = model.predict(X)
+    train_score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
+
+    X = vectorizer.transform(task2.dataset.load_tokenized_as_texts(key, 'trial'))
+    labels_gold = task2.dataset.load_labels(key, 'trial')
+
+    X = feat_transformer.transform(X)
+    labels_predict = model.predict(X)
+    trial_score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
+
+    print('[TRAIN]', train_score_dict)
+    print('[TRIAL]', trial_score_dict)
 
 
 if __name__ == '__main__':
