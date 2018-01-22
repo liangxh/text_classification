1a7e9d2245130903dc439f84e7d8e2af692e6b3d
xihaoliang <xihao.liang@ihandysoft.com>
2018-01-22 23:30:30 +0800

    <feat> knn added

--- task2/svm.py
+++ task2/svm.py
@@ -7,106 +7,45 @@ from sklearn.svm import LinearSVC, SVC
 import task2
 from task2.lib import evaluate
 from scipy import sparse
-from collections import defaultdict
-import math
-from sklearn.feature_selection import SelectFromModel
 from sklearn.multiclass import OneVsRestClassifier
 
 
 @commandr.command('linear')
-def linear(key, c=1.):
+def linear(key, c=1., lexicon=0.):
     """
     線性SVM
     輸入只用Tf-Idf
     """
     c = float(c)
+    use_lexicon = float(lexicon) == 1
+    print('use_lexicon: ', use_lexicon)
+
     tokenizer = TweetTokenizer(preserve_case=False, reduce_len=True, strip_handles=False).tokenize
     vectorizer = TfidfVectorizer(strip_accents="unicode", analyzer="word", tokenizer=tokenizer, stop_words=None)
 
     X = vectorizer.fit_transform(task2.dataset.load_tokenized_as_texts(key, 'train'))
+    if use_lexicon:
+        lexicon_feat = task2.dataset.load_lexicon_feature(key, 'train')
+        X = sparse.hstack([X, lexicon_feat])
+
     labels_gold = task2.dataset.load_labels(key, 'train')
 
     model = LinearSVC(C=c, verbose=1)
     model.fit(X, labels_gold)
     labels_predict = model.predict(X)
-    score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
-    print('[TRAIN]', score_dict)
+    train_score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
 
     X = vectorizer.transform(task2.dataset.load_tokenized_as_texts(key, 'trial'))
-    labels_gold = task2.dataset.load_labels(key, 'trial')
-    labels_predict = model.predict(X)
-    score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
-    print('[TRIAL]', score_dict)
-
-
-@commandr.command('lex')
-def lex(key, c=1.):
-    """
-    線性SVM
-    輸入只用lexicon feature
-    """
-    c = float(c)
-
-    lexicon_feat = task2.dataset.load_lexicon_feature(key, 'train')
-    X = lexicon_feat
-
-    labels_gold = task2.dataset.load_labels(key, 'train')
-    model = LinearSVC(C=c, verbose=1)
-    model.fit(X, labels_gold)
-    labels_predict = model.predict(X)
-    score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
-    print('[TRAIN]', score_dict)
-
-    lexicon_feat = task2.dataset.load_lexicon_feature(key, 'trial')
-    X = lexicon_feat
+    if use_lexicon:
+        lexicon_feat = task2.dataset.load_lexicon_feature(key, 'trial')
+        X = sparse.hstack([X, lexicon_feat])
 
     labels_gold = task2.dataset.load_labels(key, 'trial')
     labels_predict = model.predict(X)
-    score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
-    print('[TRIAL]', score_dict)
+    trial_score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
 
-
-@commandr.command('llex')
-def llex(key, c=1., weight=1.):
-    """
-    線性SVM
-    輸入使用Tf-Idf + lexicon_feature
-    """
-    c = float(c)
-    tokenizer = TweetTokenizer(preserve_case=False, reduce_len=True, strip_handles=False).tokenize
-    vectorizer = TfidfVectorizer(strip_accents="unicode", analyzer="word", tokenizer=tokenizer, stop_words=None)
-
-    X = vectorizer.fit_transform(task2.dataset.load_tokenized_as_texts(key, 'train'))
-    lexicon_feat = task2.dataset.load_lexicon_feature(key, 'train')
-    X = sparse.hstack([X, lexicon_feat])
-
-    labels_gold = task2.dataset.load_labels(key, 'train')
-
-    class_weight = weight != 0
-    if class_weight:
-        class_count = defaultdict(lambda: 0.)
-        for label in labels_gold:
-            class_count[label] += 1
-        class_weights = dict()
-        for label, count in class_count.items():
-            class_weights[label] = 1./count
-
-        model = LinearSVC(C=c, verbose=1, class_weight=class_weights)
-    else:
-        model = LinearSVC(C=c, verbose=1)
-
-    model.fit(X, labels_gold)
-    labels_predict = model.predict(X)
-    score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
-    print('[TRAIN]', score_dict)
-
-    X = vectorizer.transform(task2.dataset.load_tokenized_as_texts(key, 'trial'))
-    lexicon_feat = task2.dataset.load_lexicon_feature(key, 'trial')
-    X = sparse.hstack([X, lexicon_feat])
-    labels_gold = task2.dataset.load_labels(key, 'trial')
-    labels_predict = model.predict(X)
-    score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
-    print('[TRIAL]', score_dict)
+    print('[TRAIN]', train_score_dict)
+    print('[TRIAL]', trial_score_dict)
 
 
 @commandr.command('csupport')
@@ -133,24 +72,25 @@ def csupport(key, c=1., kernel='rbf'):
     print('[TRIAL]', score_dict)
 
 
-@commandr.command('stack')
-def stack(key, c=1.):
+@commandr.command('pca')
+def stack(key, c=1., n_components=10):
     """
     線性SVM
     輸入只用Tf-Idf
     """
+    from sklearn import decomposition
     c = float(c)
+    n_components = int(n_components)
     tokenizer = TweetTokenizer(preserve_case=False, reduce_len=True, strip_handles=False).tokenize
     vectorizer = TfidfVectorizer(strip_accents="unicode", analyzer="word", tokenizer=tokenizer, stop_words=None)
+    pca = decomposition.SparsePCA(n_components=n_components)
 
     X = vectorizer.fit_transform(task2.dataset.load_tokenized_as_texts(key, 'train'))
     labels_gold = task2.dataset.load_labels(key, 'train')
 
-    model = LinearSVC(C=c, verbose=1)
-    model.fit(X, labels_gold)
-    feat_transformer = SelectFromModel(model, prefit=True)
+    pca.fit(X)
+    X = pca.transform(X)
 
-    X = feat_transformer.transform(X)
     model = LinearSVC(C=c, verbose=1)
     model.fit(X, labels_gold)
 
@@ -158,9 +98,9 @@ def stack(key, c=1.):
     train_score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
 
     X = vectorizer.transform(task2.dataset.load_tokenized_as_texts(key, 'trial'))
+    X = pca.transform(X)
     labels_gold = task2.dataset.load_labels(key, 'trial')
 
-    X = feat_transformer.transform(X)
     labels_predict = model.predict(X)
     trial_score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
 
@@ -190,6 +130,42 @@ def one_vs_rest(key, c=1.):
     X = vectorizer.transform(task2.dataset.load_tokenized_as_texts(key, 'trial'))
     labels_gold = task2.dataset.load_labels(key, 'trial')
 
+    labels_predict = model.predict(X)
+    trial_score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
+
+    print('[TRAIN]', train_score_dict)
+    print('[TRIAL]', trial_score_dict)
+
+
+@commandr.command('knn')
+def knn(key, n_neighbors=15, weights='uniform', lexicon=0.):
+    from sklearn.neighbors import KNeighborsClassifier
+
+    use_lexicon = float(lexicon) == 1.
+    print('use_lexicon: ', use_lexicon)
+
+    tokenizer = TweetTokenizer(preserve_case=False, reduce_len=True, strip_handles=False).tokenize
+    vectorizer = TfidfVectorizer(strip_accents="unicode", analyzer="word", tokenizer=tokenizer, stop_words=None)
+
+    X = vectorizer.fit_transform(task2.dataset.load_tokenized_as_texts(key, 'train'))
+
+    if use_lexicon:
+        lexicon_feat = task2.dataset.load_lexicon_feature(key, 'train')
+        X = sparse.hstack([X, lexicon_feat])
+
+    labels_gold = task2.dataset.load_labels(key, 'train')
+
+    model = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
+    model.fit(X, labels_gold)
+    labels_predict = model.predict(X)
+    train_score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
+
+    X = vectorizer.transform(task2.dataset.load_tokenized_as_texts(key, 'trial'))
+    if use_lexicon:
+        lexicon_feat = task2.dataset.load_lexicon_feature(key, 'trial')
+        X = sparse.hstack([X, lexicon_feat])
+
+    labels_gold = task2.dataset.load_labels(key, 'trial')
     labels_predict = model.predict(X)
     trial_score_dict = evaluate.score(labels_predict=labels_predict, labels_gold=labels_gold)
 
